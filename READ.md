38ebf7e45e43e9653b37bb3a07a19329b6524c9b
nurinarahayu1827 <nurinarahayu0406@gmail.com>
2021-05-27 13:03:21 +0800

    Membuat Database Class

---
+++ READ.md
@@ -0,0 +1,1 @@
+"# Tugas PMA Room DB" 
---
+++ app/src/main/java/id/ac/unhas/belajarroomdb/AppDatabase.kt
@@ -0,0 +1,35 @@
+package id.ac.unhas.belajarroomdb
+
+import android.content.Context
+import android.provider.ContactsContract
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [ContactsContract.CommonDataKinds.Note::class], exportSchema = false, version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        private const val DB_NAME = "NOTE_DB"
+        private var instance : AppDatabase? = null
+
+        @InternalCoroutinesApi
+        fun  geInstance(context: Context): AppDatabase? {
+            if (instance == null) {
+                synchronized(AppDatabase::class) {
+                    instance = Room
+                        .databaseBuilder(
+                            context,
+                            AppDatabase::class.java,
+                            DB_NAME
+                        )
+                        .build()
+                }
+            }
+            return instance
+        }
+    }
+}
